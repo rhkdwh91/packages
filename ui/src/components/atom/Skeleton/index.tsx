c28c241dcbb074a55d2eedb99acb916eb073f746
MacAir.Kwangyeon <rhkdwh91@gmail.com>
2024-08-15 13:02:02 +0900

    Refactor import and type usage in Skeleton component
    Replace default import of HTMLAttributes with a namespaced React import and modify Skeleton function to use React.HTMLAttributes for cleaner and more consistent code. This aligns the import style with other components and ensures better readability.

--- ui/src/components/atom/Skeleton/index.tsx
+++ ui/src/components/atom/Skeleton/index.tsx
@@ -1,7 +1,10 @@
-import { HTMLAttributes } from "React";
+import * as React from "react";
 import { cn } from "lib/utils";
 
-function Skeleton({ className, ...props }: HTMLAttributes<HTMLDivElement>) {
+function Skeleton({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
       className={cn("animate-pulse rounded-md bg-muted", className)}
